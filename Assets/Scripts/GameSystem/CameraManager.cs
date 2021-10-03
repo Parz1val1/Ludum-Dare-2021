b9986308990726987e9c43572da21389f9d9e375
Michael Williams <msw201089@gmail.com>
2021-10-03 01:12:38 -0400

    added in infiniteness
    woohoo, it r be an infinite runner now

---
+++ Assets/Scripts/GameSystem/CameraManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameSystem
+{
+    public class CameraManager : MonoBehaviour
+    {
+        public GameObject _frontSpawnPoint;
+        public GameObject _backSpawnPoint;
+    }
+}
